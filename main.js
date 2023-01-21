c2ada85242974ec55a2043e6b6ea90f9911cd8d6
JULIA TEIXEIRA <juliarochateixeira@gmail.com>
2023-01-21 12:19:16 -0300

    finalizando o projeto

--- main.js
+++ main.js
@@ -3,26 +3,22 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector('header button')
 
 button.addEventListener('click', add)
+form.addEventListener('change', save)
 
 function add() {
     const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
     const dayExists = nlwSetup.dayExists(today)
-    
     if(dayExists) {
         alert("Dia já incluso")
         return
     }
-
-    nlwSetup.addDay('01/01')
+    nlwSetup.addDay(today)
 }
 
-// const data = {
-//     run: ['01-02', '03-02'],
-//     food: ['04-02'],
-//     journal: ['02-02'],
-//     takePills: ['01-02'],
-    
-// }
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
 
-// nlwSetup.setData(data)
-// nlwSetup.load()
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
