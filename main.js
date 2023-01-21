29414a2c649971a9eacc0e377801b4eb4640363e
JULIA TEIXEIRA <juliarochateixeira@gmail.com>
2023-01-21 11:55:08 -0300

    update

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-02', '03-02'],
-    food: ['04-02'],
-    journal: ['02-02'],
-    takePills: ['01-02'],
+button.addEventListener('click', add)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
     
+    if(dayExists) {
+        alert("Dia já incluso")
+        return
+    }
+
+    nlwSetup.addDay('01/01')
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+// const data = {
+//     run: ['01-02', '03-02'],
+//     food: ['04-02'],
+//     journal: ['02-02'],
+//     takePills: ['01-02'],
+    
+// }
+
+// nlwSetup.setData(data)
+// nlwSetup.load()
